b69b2b4604094c16dfa3ec9dcda5764813a1374d
Natanael de Souza <natanaelsouzza123@gmail.com>
2026-09-13 19:42:48 -0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("androidx.documentfile:documentfile:1.0.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
